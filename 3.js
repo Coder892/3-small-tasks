d1c500c0e0a5f27338f625842a4060dbf073763e
Richa Singh <richu.1248@gmail.com>
2024-09-16 19:24:16 +0530

    second commit

---
+++ 3.js
@@ -0,0 +1,5 @@
+// Add any interactivity you want for your website here
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your message has been sent!');
+  });
